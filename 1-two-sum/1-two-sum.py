21df405105598c9173c2720b8c92ee7ba2a04eeb
Rut092 <52249937+Rut092@users.noreply.github.com>
2022-05-23 22:25:41 +0530

    Time: 95 ms (55.64%), Space: 15.3 MB (14.44%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict={}
         for i in range(len(nums)):
-            alpha=nums
-            no=nums[i]
-            alpha[i]=-99999999
-            if target-no in alpha:
-                return [i,alpha.index(target-no)]
+            a=target-nums[i]
+            if a in dict:
+                return [dict[a],i]
+            else:
+                dict[nums[i]]=i
                 
                 
