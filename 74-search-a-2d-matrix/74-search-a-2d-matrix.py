96c6e5da652288a03ae247a8212ee62746e1bdde
Rut092 <52249937+Rut092@users.noreply.github.com>
2022-06-20 17:19:17 +0530

    Time: 87 ms (14.12%), Space: 14.4 MB (42.33%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -5,9 +5,6 @@ class Solution:
         n=len(matrix[0])
         high=m*n-1
         mid=(high+0)//2
-        print(m)
-        #if m==1:
-        #    matrix.append([-1])
         while(high>=low):
             if matrix[low//n][low%n]==target:
                 return True
