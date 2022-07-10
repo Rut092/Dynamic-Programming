4af1a2056ace617cbf6dc492af6b61d277661b97
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-07-10 15:49:35 +0530

    Time: 140 ms (75.78%), Space: 13.9 MB (94.93%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        dp=[[0 for i in range(2*k+1)] for j in range(len(prices)+1)]
+        prev=[0 for i in range(2*k+1)]
+        curr=prev.copy()
         
         for ind in range(len(prices)-1,-1,-1):
             for trans in range(2*k-1,-1,-1):
                 
                 if trans%2==0:
-                    dp[ind][trans]=max( -prices[ind]+dp[ind+1][trans+1], dp[ind+1][trans])
+                    prev[trans]=max( -prices[ind]+curr[trans+1], curr[trans])
                 else:
-                    dp[ind][trans]=max( prices[ind]+dp[ind+1][trans+1], dp[ind+1][trans])
-        return dp[0][0]
+                    prev[trans]=max(prices[ind]+curr[trans+1], curr[trans])
+            curr=prev
+        return prev[0]
