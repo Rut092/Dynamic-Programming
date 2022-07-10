813798c3687cc072602f9279631a2432e3c2406d
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-07-10 14:15:15 +0530

    Time: 4506 ms (13.90%), Space: 46.2 MB (19.89%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,28 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp=[[[-9999 for p in range(3)] for i in range(len(prices)+1)] for j in range(2)]
+        dp=[[[0 for p in range(3)] for i in range(len(prices)+1)] for j in range(2)]
         
-        def f(ind,buy,cap):
-            
-            if cap==0:
-                return 0
-            
-            if ind==len(prices):
-                return 0
-            
-            elif dp[buy][ind][cap]!=-9999:
-                return dp[buy][ind][cap]
-            
-            else:
-                if buy:
-                    profit= max(-prices[ind]+f(ind+1,0,cap),f(ind+1,1,cap))
+        for ind in range(len(prices)-1,-1,-1):
+            for buy in range(2):
+                for cap in range(1,3):
                     
-                else:
-                    profit= max(prices[ind]+f(ind+1,1,cap-1),f(ind+1,0,cap))
-                    
-                dp[buy][ind][cap]= profit
-                return profit
-            
-            
-            
-        return f(0,1,2)
+                    if buy:
+                        dp[buy][ind][cap]=max(-prices[ind]+dp[0][ind+1][cap],dp[1][ind+1][cap])
+                    else:
+                        dp[buy][ind][cap]=max(prices[ind]+dp[1][ind+1][cap-1],dp[0][ind+1][cap])
+                        
+        return dp[1][0][2]
