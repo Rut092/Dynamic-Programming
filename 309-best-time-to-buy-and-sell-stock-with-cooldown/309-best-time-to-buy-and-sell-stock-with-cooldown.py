59404088bfa7805346da0d3f72b4b72228b95242
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-07-10 16:21:30 +0530

    Time: 48 ms (87.37%), Space: 14.6 MB (46.72%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,10 +4,7 @@ class Solution:
         dp=[[0 for i in range(2)]for j in range(len(prices)+2)]
         
         for ind in range(len(prices)-1,-1,-1):
-            for buy in range(2):
+                dp[ind][1]=max( -prices[ind]+dp[ind+1][0],dp[ind+1][1])
                 
-                if buy:
-                    dp[ind][buy]=max( -prices[ind]+dp[ind+1][0],dp[ind+1][1])
-                else:
-                    dp[ind][buy]=max( prices[ind]+dp[ind+2][1],dp[ind+1][0])
+                dp[ind][0]=max( prices[ind]+dp[ind+2][1],dp[ind+1][0])
         return dp[0][1]
