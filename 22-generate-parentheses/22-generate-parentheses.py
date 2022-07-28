eb0be346a5cde1bfd5aa1ab1e6aae68bc0b7cdac
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-07-28 18:31:26 +0530

    Time: 83 ms (9.39%), Space: 14.3 MB (39.71%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        a=['(',')']
+        arr=[]
+        temp=''
+        def f(open,close,temp):
+            if open==n and close==n:
+                arr.append(temp)
+            elif open==n:
+                temp+=')'
+                f(open,close+1,temp)
+            else:
+                temp+='('
+                f(open+1,close,temp)
+                temp=temp[:-1]
+                if open>close:
+                    temp+=')'
+                    f(open,close+1,temp)
+                    
+        f(0,0,temp)
+        return arr
