e215e40daa867b485d1a14240d8533657c35daee
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-02 00:55:01 +0530

    Time: 174 ms (85.91%), Space: 24.3 MB (18.31%) - LeetHub

--- 403-frog-jump/403-frog-jump.py
+++ 403-frog-jump/403-frog-jump.py
@@ -4,16 +4,12 @@ class Solution:
         @cache
         def f(val,prev):
             if val==prev:
-                return False
-            
+                return False 
             if val==stones[-1]:
                 return True
-            
             if val in s:
-                
                 if f(val+(val-prev-1),val) or f(val+(val-prev),val) or f(val+(val-prev+1),val):
                     return True
-                
         if stones[1]-stones[0]>1:
             return False
         return f(stones[1],stones[0])
