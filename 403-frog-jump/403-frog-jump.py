1c20b9ea3ac3a2687220e89f52431fecf8f96669
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-02 00:53:10 +0530

    Time: 298 ms (48.73%), Space: 24 MB (19.83%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        s=set(stones)
+        @cache
+        def f(val,prev):
+            if val==prev:
+                return False
+            
+            if val==stones[-1]:
+                return True
+            
+            if val in s:
+                
+                if f(val+(val-prev-1),val) or f(val+(val-prev),val) or f(val+(val-prev+1),val):
+                    return True
+                
+        if stones[1]-stones[0]>1:
+            return False
+        return f(stones[1],stones[0])
+        
