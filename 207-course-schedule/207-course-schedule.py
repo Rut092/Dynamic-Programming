6bc323b7f92e6da0337a7ae6f4a56907318a510d
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-10 01:34:42 +0530

    Time: 194 ms (24.05%), Space: 15.3 MB (96.86%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dict=[[] for i in range(numCourses)]
+        indeg=[0]*numCourses
+
+        for i in prerequisites:
+            if i[0]==i[1]:
+                return False
+            dict[i[0]].append(i[1])
+            indeg[i[1]]+=1
+                
+        temp=[]
+        for i in range(numCourses):
+            if indeg[i]==0:
+                temp.append(i)
+        
+        count=0
+        while(len(temp)!=0):
+            a=temp.pop(0)
+            count+=1
+            for i in dict[a]:
+                indeg[i]-=1
+                if indeg[i]==0:
+                    temp.append(i)
+            
+        print(count)
+        if count==numCourses:
+            return True
+        else:
+            return False
