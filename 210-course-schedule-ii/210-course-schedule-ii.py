c5eb49b516b14e012dc836136ba93f907bb0c0af
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-11 17:45:58 +0530

    Time: 152 ms (59.81%), Space: 15.4 MB (97.07%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dict=[[] for i in range(numCourses)]
+        indeg=[0]*numCourses
+
+        for i in prerequisites:
+            if i[0]==i[1]:
+                return []
+            dict[i[0]].append(i[1])
+            indeg[i[1]]+=1
+                
+        temp=[]
+        for i in range(numCourses):
+            if indeg[i]==0:
+                temp.append(i)
+        
+        count=0
+        ans=temp.copy()
+        while(len(temp)!=0):
+            a=temp.pop(0)
+            count+=1
+            for i in dict[a]:
+                indeg[i]-=1
+                if indeg[i]==0:
+                    temp.append(i)
+                    ans.append(i)
+
+        if count==numCourses:
+            return ans[::-1]
+        else:
+            return []
