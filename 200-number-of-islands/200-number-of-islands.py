726a85a7527e65bb2aa888d41969f475a65b8b8e
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-11 17:28:18 +0530

    Time: 673 ms (14.64%), Space: 16.3 MB (91.03%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m=len(grid[0])
+        n=len(grid)
+        vis=[[0 for i in range(m)] for j in range(n)]
+        
+        def bfs(i,j):
+            vis[i][j]=1
+            queue=[[i,j]]
+            
+            while(len(queue)!=0):
+                no=queue.pop(0)
+                fst=no[0]
+                lst=no[1]
+                ind2=lst
+                for ind1 in range(fst-1,fst+2):
+                    if ind1>=0 and ind1<n and grid[ind1][ind2]=='1' and vis[ind1][ind2]==0:
+                        vis[ind1][ind2]=1
+                        queue.append([ind1,ind2])
+                ind1=fst
+                for ind2 in range(lst-1,lst+2):
+                    if ind2>=0 and ind2<m and grid[ind1][ind2]=='1' and vis[ind1][ind2]==0:
+                        vis[ind1][ind2]=1
+                        queue.append([ind1,ind2])
+        
+        count=0
+        for i in range(n):
+            for j in range(m):
+                if vis[i][j]==0 and grid[i][j]=='1':
+                    count+=1
+                    bfs(i,j)
+        return count
