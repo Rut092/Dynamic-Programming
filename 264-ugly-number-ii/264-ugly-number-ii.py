949c2034ee15688330a2ec3e9e781683e37497f5
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-17 01:03:07 +0530

    Time: 253 ms (52.96%), Space: 13.9 MB (54.09%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        dp=[0 for i in range(n)]
+        dp[0]=1
+        p1=0
+        p2=0
+        p3=0
+        for i in range(1,n):
+            dp[i]=min(2*dp[p1],3*dp[p2],5*dp[p3])
+            if dp[i]==2*dp[p1]:
+                p1+=1
+            if dp[i]==3*dp[p2]:
+                p2+=1
+            if dp[i]==5*dp[p3]:
+                p3+=1
+        return dp[n-1]
