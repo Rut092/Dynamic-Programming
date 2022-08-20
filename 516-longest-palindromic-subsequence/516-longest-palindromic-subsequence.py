bb29cd9bb12a0bd13f2dfc07732dd42a520b7b9f
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-08-21 02:13:21 +0530

    Time: 5105 ms (18.04%), Space: 38.9 MB (46.68%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -6,8 +6,6 @@ class Solution:
             for j in range(1,a+1):
                 if s[i-1]==s[a-j]:
                     dp[i][j]=1+dp[i-1][j-1]
-                    
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-                    
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])     
         return dp[-1][-1]
