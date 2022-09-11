9783e0d06f25769741c37c1a7e5ed200018a52d1
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2022-09-11 23:15:43 +0530

    Time: 1216 ms (25.89%), Space: 16.3 MB (73.31%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -5,8 +5,6 @@ class Solution:
         b=len(matrix)
         count=0
         
-        dp=matrix.copy()
-        
         for i in range(b):
             count+=matrix[i][0]
             
@@ -17,7 +15,7 @@ class Solution:
             for j in range(1,a):
                 
                 if matrix[i][j]>0:
-                    dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
-                count+=dp[i][j]
+                    matrix[i][j]=min(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1])+1
+                count+=matrix[i][j]
 
         return count
